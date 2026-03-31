d9fb79f8a9a10f592f85c042ceea6c0bbbf8b31d
hsebirk-droid <hsebirk@gmail.com>
2026-03-31 21:46:17 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,6 @@
-// Configuração do Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getFirestore, collection, doc, getDoc, getDocs, addDoc, updateDoc, deleteDoc, query, where, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAwywvboQgWqkrZlbwrUPdaUq-gLnWM64E",
   authDomain: "plataforma-ssa-3130e.firebaseapp.com",
@@ -8,21 +10,11 @@ const firebaseConfig = {
   appId: "1:850671249674:web:53a115408f8006745c32f3"
 };
 
-// Inicializar Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-import { getFirestore, collection, doc, getDoc, getDocs, addDoc, updateDoc, deleteDoc, query, where, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-storage.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const storage = getStorage(app);
-const auth = getAuth(app);
 
-// Exportar para uso em outros ficheiros
 export { 
-  db, storage, auth,
+  db, 
   collection, doc, getDoc, getDocs, addDoc, updateDoc, deleteDoc,
-  query, where, orderBy, onSnapshot,
-  ref, uploadBytes, getDownloadURL
+  query, where, orderBy, onSnapshot
 };
